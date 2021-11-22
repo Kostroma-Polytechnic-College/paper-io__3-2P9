53559c53641ff072202f4bab6c692d232b16c14c
ChervontsevaAnna <anna.chervontseva@bk.ru>
2021-11-22 11:00:26 +0300

    123

---
+++ paper-io/Room.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace paper_io
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        enum Direction { left, right, up, down, none };
+        double x = 0;
+        double y = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(MovePlayer);
+            timer.Start();
+        }
+        private void MovePlayer(object sender, EventArgs e)
+        {
+            if (Keyboard.IsKeyDown(Key.Left))
+            {
+                x -= .05;
+                Canvas.SetLeft(Player, x);
+            }
+            if (Keyboard.IsKeyDown(Key.Up))
+            {
+                y -= .05;
+                Canvas.SetTop(Player, y);
+            }
+            if (Keyboard.IsKeyDown(Key.Down))
+            {
+                y += .05;
+                Canvas.SetTop(Player, y);
+            }
+            if (Keyboard.IsKeyDown(Key.Right))
+            {
+                x += .05;
+                Canvas.SetLeft(Player, x);
+            }
+        }
+        //private void MoveCamera(object sender, EventArgs e)
+        //{
+        //    if (Keyboard.IsKeyDown(Key.Left))
+        //    {
+        //        x -= .05;
+        //        Canvas.SetLeft(CanvasViewer, x);
+        //    }
+        //    if (Keyboard.IsKeyDown(Key.Up))
+        //    {
+        //        y -= .05;
+        //        Canvas.SetTop(CanvasViewer, y);
+        //    }
+        //    if (Keyboard.IsKeyDown(Key.Down))
+        //    {
+        //        y += .05;
+        //        Canvas.SetTop(CanvasViewer, y);
+        //    }
+        //    if (Keyboard.IsKeyDown(Key.Right))
+        //    {
+        //        x += .05;
+        //        Canvas.SetLeft(CanvasViewer, x);
+        //    }
+        //}
+    }
+}
