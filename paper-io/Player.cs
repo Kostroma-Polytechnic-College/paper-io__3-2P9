02a9bed081d7532f6c36540c84a048db0851ced3
ChervontsevaAnna <anna.chervontseva@bk.ru>
2021-12-08 09:13:50 +0300

    paper-io

--- paper-io/Player.cs
+++ paper-io/Player.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace paper_io
 {
@@ -11,5 +12,21 @@ namespace paper_io
     /// </summary>
     public class Player
     {
+        Point location = new Point();
+        List<Point> plume = new List<Point>();
+
+        Boolean areNotEqual = true;
+
+        public bool CheckdDeathCondition(Point Players)
+        {
+            foreach (Point item in plume)
+                if (item.X == location.X && item.Y == location.Y)
+                    return true;
+
+
+
+
+            return false;
+        }
     }
 }
