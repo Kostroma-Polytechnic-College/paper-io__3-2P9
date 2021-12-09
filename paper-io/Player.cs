db37298edb5e02d4a1d5cbe318b24e6f45f31de0
ChervontsevaAnna <anna.chervontseva@bk.ru>
2021-12-09 12:33:19 +0300

    added something

---
+++ paper-io/Player.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace paper_io
+{
+    /// <summary>
+    /// Класс, отвечающий за логику игрока
+    /// </summary>
+    public class Player
+    {
+        enum Direction
+        {
+            Left,
+            Right,
+            Down,
+            Up
+        }
+        Direction direction;
+        public Player(byte rand)
+        {
+            
+        }
+
+        Point location = new Point();
+        List<Point> plume = new List<Point>();
+
+        Boolean areNotEqual = true;
+
+        public bool CheckdDeathCondition(Point Players)
+        {
+            foreach (Point item in plume)
+                if (item.X == location.X && item.Y == location.Y)
+                    return true;
+            return false;
+        }
+        int m;
+        int k;
+        int c; // свободная территория
+        int q; // вражеская территория
+
+        public void Step(Game[,] room)
+        {
+            if (location.X == c && location.Y == c && location.X == q && location.Y == q)
+            {
+                plume.Add(location.X);
+            }
+        }
+
+        public void Bot(Game[,] room)
+        {
+            ///Если со всех сторон находится территория текущего игрока, то направление движения не менять. 
+            if (location.X + 1 == m && location.X - 1 == m && location.Y + 1 == m && location.Y - 1 == m)
+            {
+
+            }
+            ///Если впереди текущего игрока находится стена и слева нет стены, то повернуть на лево.
+            else if ()
+            {
+
+            }
+            ///Если впереди игрока находится стена и справа нет стены, то повернуть направо.
+            else if ()
+            {
+
+            }
+            ///Если впереди и слева и справа нет территории текущего игрока, то повернуть направо если там нет стены или повернуть налево если справа есть стена.
+            else if ()
+            {
+                if ()
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            ///Если впереди нет территории текущего игрока, а слево или справа есть территория текущего игрока, то повернуть в сторону территории текущего игрока.
+            else if ()
+            {
+
+            }
+
+        }
+    }
+}
---
+++ paper-io/Start.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace paper_io
+{
+    /// <summary>
+    /// Логика взаимодействия для Start.xaml
+    /// </summary>
+    public partial class Start : Window
+    {
+        public Start()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// При нажатии на кнопку происходит проверка количества игроков, отправка количества игроков в класс Game и запуск окна с игрой Room
+        /// </summary>
+        /// <param name="sender">Предоставляет ссылку на кнопку</param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            byte n;
+            bool success = byte.TryParse(Gamers.Text, out n);
+            if (success)
+            {
+                if (n <= 1 | n>10)
+                {
+                    Error.Content = "Введите число от 2 до 10";
+                }
+                else
+                {
+                    /// используеться для перехода к окну с игрой и передачи количества игроков
+                    Room room = new Room(n);
+                    this.Hide();
+                    room.ShowDialog();
+                    this.Show();
+                }
+            }
+            else
+            { 
+                Error.Content = "Введите число от 2 до 10";
+            }
+        }
+    }
+}
