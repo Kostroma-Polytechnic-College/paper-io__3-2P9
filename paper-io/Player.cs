bf32e7fd5883d41595dd38fa9296f331e76bed0a
ChervontsevaAnna <anna.chervontseva@bk.ru>
2021-12-13 14:39:45 +0300

    fgh

--- paper-io/Player.cs
+++ paper-io/Player.cs
@@ -3,30 +3,56 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 using System.Windows;
 
 namespace paper_io
 {
     /// <summary>
-    /// Класс, отвечающий за логику игрока
+    /// Заглушка класса Player
     /// </summary>
     public class Player
     {
-        Point location = new Point();
+        /// <summary>
+        /// 
+        /// </summary>
+        public Point location = new Point();
+        /// <summary>
+        /// 
+        /// </summary>
         List<Point> plume = new List<Point>();
 
         Boolean areNotEqual = true;
-
-        public bool CheckdDeathCondition(Point Players)
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="players"></param>
+        /// <param name="Room"></param>
+        /// <returns></returns>
+        public bool CheckdDeathCondition(List<Player> players, Player[,] Room)
         {
-            foreach (Point item in plume)
-                if (item.X == location.X && item.Y == location.Y)
-                    return true;
 
+            foreach (Player playerItem in players)
+            {
+                foreach (Point plumeItem in plume)
+                {
 
-
-
+                    if (playerItem.location.X == plumeItem.X && playerItem.location.Y == plumeItem.Y)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (location.X < 0 || location.X > Room.GetLength(1) || location.Y < 0 || location.Y > Room.GetLength(0))
+            {
+                return true;
+            }
+            if ()
+            {
+                return true;
+            }
             return false;
         }
+
     }
 }
