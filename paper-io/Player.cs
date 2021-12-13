8349da93db3c98f8abe33a58f1b08a9b946d62a5
ChervontsevaAnna <anna.chervontseva@bk.ru>
2021-12-13 15:17:25 +0300

    возможно завершено

--- paper-io/Player.cs
+++ paper-io/Player.cs
@@ -14,24 +14,22 @@ namespace paper_io
     public class Player
     {
         /// <summary>
-        /// 
+        /// положение игрока
         /// </summary>
         public Point location = new Point();
         /// <summary>
-        /// 
+        /// координаты шлейфа
         /// </summary>
         List<Point> plume = new List<Point>();
-
         Boolean areNotEqual = true;
         /// <summary>
-        /// 
+        /// условие смерти
         /// </summary>
-        /// <param name="players"></param>
-        /// <param name="Room"></param>
+        /// <param name="players">для проверки касания шлейфа</param>
+        /// <param name="Room">для проверки наличия территории игрока и проверки выезда игрока за карту</param>
         /// <returns></returns>
         public bool CheckdDeathCondition(List<Player> players, Player[,] Room)
         {
-
             foreach (Player playerItem in players)
             {
                 foreach (Point plumeItem in plume)
@@ -47,12 +45,14 @@ namespace paper_io
             {
                 return true;
             }
-            if ()
+            foreach (Player territory in Room)
             {
-                return true;
+                if (this == territory)
+                {
+                }
+                else return true;
             }
             return false;
         }
-
     }
 }
