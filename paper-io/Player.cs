61ff793d9ce64b87e3f93874cd7ed73e6be0963e
istrebitelantox <geekpook@gmail.com>
2022-01-20 10:39:48 +0300

    Change

--- paper-io/Player.cs
+++ paper-io/Player.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-///<summary> 
-///Это пространво имён необходимо для реализации структуры Point 
-///</summary>
-using System.Windows;
+using System.Drawing;
 
 namespace paper_io
 {
@@ -25,6 +22,9 @@ namespace paper_io
     /// </summary>
     public class Player
     {
+        /// <summary>
+        /// Создание поля
+        /// </summary>
         public Direction Direction;
         /// <summary>
         /// Метод, реализующий поворот налево
@@ -58,37 +58,6 @@ namespace paper_io
         /// координаты шлейфа
         /// </summary>
         List<Point> plume = new List<Point>();
-        /// <summary>
-        /// условие смерти
-        /// </summary>
-        /// <param name="players">для проверки касания шлейфа</param>
-        /// <param name="Room">для проверки наличия территории игрока и проверки выезда игрока за карту</param>
-        /// <returns></returns>
-        public bool CheckdDeathCondition(List<Player> players, Player[,] Room)
-        {
-            foreach (Player playerItem in players)
-            {
-                foreach (Point plumeItem in plume)
-                {
-                    if (playerItem.location.X == plumeItem.X && playerItem.location.Y == plumeItem.Y)
-                    {
-                        return true;
-                    }
-                }
-            }
-            if (location.X < 0 || location.X > Room.GetLength(1) || location.Y < 0 || location.Y > Room.GetLength(0))
-            {
-                return true;
-            }
-            foreach (Player territory in Room)
-            {
-                if (this == territory)
-                {
-                }
-                else return true;
-            }
-            return false;
-        }
         /// <summary>
         /// Метод, отвечающий за изменение направления бота
         /// </summary>
